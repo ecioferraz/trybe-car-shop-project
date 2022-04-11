c84b3fd999a2a9c42ae30da38282789b50c8f633
Écio Ferraz <ecioferraz@hotmail.com>
2022-04-11 17:08:02 -0300

    customRouter

---
+++ src/routes/Router.ts
@@ -0,0 +1,18 @@
+import { Router } from 'express';
+import Controller from '../controllers';
+
+export default class CustomRouter<T> {
+  public router: Router;
+
+  constructor() {
+    this.router = Router();
+  }
+
+  public addRouter(
+    controller: Controller<T>,
+    route: string = controller.route,
+  ) {
+    this.router.route(route)
+      .get(controller.create);
+  }
+}
