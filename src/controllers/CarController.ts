94211241069feea206e6e21b2211cc67ca6afad0
Écio Ferraz <ecioferraz@hotmail.com>
2022-04-12 09:35:38 -0300

    reformatting

--- src/controllers/CarController.ts
+++ src/controllers/CarController.ts
@@ -1,4 +1,4 @@
-import { Response } from 'express';
+import { Request, Response } from 'express';
 import Controller, { IRequestWithBody, ResponseError } from '.';
 import CarService from '../services/CarService';
 import { Car } from '../interfaces/CarInterface';
@@ -20,13 +20,38 @@ export default class CarController extends Controller<Car> {
     try {
       const car = await this.service.create(req.body);
 
-      if (!car) return res.status(500).json({ error: this.errors.internal });
+      if (!car) {
+        return res
+          .status(this.status.INTERNAL)
+          .json({ error: this.errors.internal });
+      }
 
-      if ('error' in car) return res.status(400).json(car);
+      if ('error' in car) return res.status(this.status.BAD_REQUEST).json(car);
 
-      return res.status(201).json(car);
+      return res.status(this.status.CREATED).json(car);
     } catch (error) {
-      return res.status(500).json({ error: this.errors.internal });
+      return res
+        .status(this.status.INTERNAL)
+        .json({ error: this.errors.internal });
+    }
+  };
+
+  public readOne = async (
+    req: Request<{ id: string }>,
+    res: Response<Car | ResponseError>,
+  ): Promise<typeof res> => {
+    try {
+      const { id } = req.params;
+      const car = await this.service.readOne(id);
+
+      return car ? res.json(car)
+        : res
+          .status(this.status.NOT_FOUND)
+          .json({ error: this.errors.notFound });
+    } catch (error) {
+      return res
+        .status(this.status.INTERNAL)
+        .json({ error: this.errors.internal });
     }
   };
 }
--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -16,10 +16,20 @@ enum ControllerErros {
   badRequest = 'Bad request',
 }
 
+enum StatusCode {
+  OK = 200,
+  CREATED,
+  BAD_REQUEST = 400,
+  NOT_FOUND = 404,
+  INTERNAL = 500,
+}
+
 export default abstract class Controller<T> {
   abstract route: string;
 
   protected errors = ControllerErros;
+
+  protected status = StatusCode;
 
   constructor(protected service: Service<T>) { }
 
@@ -37,7 +47,14 @@ export default abstract class Controller<T> {
 
       return res.json(objs);
     } catch (error) {
-      return res.status(500).json({ error: this.errors.internal });
+      return res
+        .status(this.status.INTERNAL)
+        .json({ error: this.errors.internal });
     }
   };
+
+  public abstract readOne(
+    req: Request<{ id: string }>,
+    res: Response<T | ResponseError>,
+  ): Promise<typeof res>;
 }
