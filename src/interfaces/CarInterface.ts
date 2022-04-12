7e22cc0d854f3e2888d98feddcf30338092e2262
Écio Ferraz <ecioferraz@hotmail.com>
2022-04-12 08:11:08 -0300

    fix validations

--- src/interfaces/CarInterface.ts
+++ src/interfaces/CarInterface.ts
@@ -5,13 +5,13 @@ const CarSchema = z.object({
   doorsQty: z.number({
     required_error: 'doorsQty is required',
     invalid_type_error: 'dorrsQty must be a number',
-  }).min(2, { message: 'Must be 2 or higher' })
-    .max(4, { message: 'Must be 4 or lower' }),
+  }).gte(2, { message: 'doorsQty must be 2 or higher' })
+    .lte(4, { message: 'doorsQty must be 4 or lower' }),
   seatsQty: z.number({
     required_error: 'doorsQty is required',
     invalid_type_error: 'dorrsQty must be a number',
-  }).min(2, { message: 'Must be 2 or higher' })
-    .max(7, { message: 'Must be 7 or lower' }),
+  }).gte(2, { message: 'doorsQty must be 2 or higher' })
+    .lte(7, { message: 'doorsQty must be 7 or lower' }),
 }).merge(VehicleSchema);
 
 type Car = z.infer<typeof CarSchema>;
--- src/interfaces/VehicleInterface.ts
+++ src/interfaces/VehicleInterface.ts
@@ -4,12 +4,12 @@ const VehicleSchema = z.object({
   model: z.string({
     required_error: 'model is required',
     invalid_type_error: 'model must be a string',
-  }).min(3, { message: 'Must be 3 or more characters' }),
+  }).min(3, { message: 'model must be 3 or more characters' }),
   year: z.number({
     required_error: 'year is required',
     invalid_type_error: 'year must be a number',
-  }).min(1900, { message: 'year must be 1900 or higher' })
-    .max(2022, { message: 'year can\'t be lower than 2022' }),
+  }).gte(1900, { message: 'year must be 1900 or higher' })
+    .lte(2022, { message: 'year can\'t be lower than 2022' }),
   color: z.string({
     required_error: 'color is required',
     invalid_type_error: 'color must be a string',
