546fb7976e40b54efc18c51d6cc0da0b9f34d715
Écio Ferraz <ecioferraz@hotmail.com>
2022-04-18 10:37:55 -0300

    finish 90% of all tests

--- src/tests/unit/controllers/controller.test.ts
+++ src/tests/unit/controllers/controller.test.ts
@@ -25,97 +25,123 @@ describe('CarController', () => {
   const carModel = new CarModel();
 
   describe('create', () => {
-    before(() => Sinon.stub(carModel.model, 'create').resolves(carMock));
+    before(() => Sinon.stub(carModel.model, 'create')
+      .onFirstCall().resolves(carMock)
+      .onSecondCall().throws()
+      .onThirdCall().throws());
   
     after(()=> Sinon.restore());
   
-    it('should return status 201 when data is valid', async () => {
+    it('should return status 201 and a new car created when data is valid', async () => {
       const chaiHttpResponse = await chai.request(app).post('/cars').send(carBodyMock);
   
       expect(chaiHttpResponse).to.have.status(201);
-    });
-  
-    it('should return a new car created when data is valid', async () => {
-      const chaiHttpResponse = await chai.request(app).post('/cars').send(carBodyMock);
-  
       expect(chaiHttpResponse.body).to.be.deep.eq(carMock);
     });
 
-    it('should return status 400 when data is invalid', async () => {
+    it('should return status 400 and a ZodError message when data is invalid', async () => {
       const chaiHttpResponse = await chai.request(app).post('/cars').send(invalidCarBodyMock);
-  
+
       expect(chaiHttpResponse).to.have.status(400);
+      expect(chaiHttpResponse.body.error.name).to.be.eq('ZodError');
+    });
+
+    it('should return status 500 and an "Internal Server Error" message', async () => {
+      const chaiHttpResponse = await chai.request(app).post('/cars').send(carMock);
+
+      expect(chaiHttpResponse).to.have.status(500);
+      expect(chaiHttpResponse.body.error).to.be.eq('Internal Server Error');
     });
   });
 
   describe('read', () => {
-    before(() => Sinon.stub(carModel.model, 'find').resolves(carListMock as any[]));
+    before(() => Sinon.stub(carModel.model, 'find')
+      .onFirstCall().resolves(carListMock as any[])
+      .onSecondCall().throws());
   
     after(()=> Sinon.restore());
   
-    it('should return status 200', async () => {
+    it('should return status 200 and a list of all cars', async () => {
       const chaiHttpResponse = await chai.request(app).get('/cars').send();
   
       expect(chaiHttpResponse).to.have.status(200);
+      expect(chaiHttpResponse.body).to.be.deep.eq(carListMock);
     });
   
-    it('should return a list of all cars', async () => {
+    it('should return status 500 and an "Internal Server Error" message', async () => {
       const chaiHttpResponse = await chai.request(app).get('/cars').send();
   
-      expect(chaiHttpResponse.body).to.be.deep.eq(carListMock);
+      expect(chaiHttpResponse).to.have.status(500);
+      expect(chaiHttpResponse.body.error).to.be.eq('Internal Server Error');
     });
   });
 
   describe('readOne', () => {
-    before(() => Sinon.stub(carModel.model, 'findById').resolves(carMock as any));
+    before(() => Sinon.stub(carModel.model, 'findById')
+      .onFirstCall().resolves(carMock as any)
+      .onSecondCall().throws()
+      .onThirdCall().throws());
   
     after(()=> Sinon.restore());
   
-    it('should return status 200 when the id is valid', async () => {
+    it('should return status 200 and a car when the id is valid', async () => {
       const chaiHttpResponse = await chai.request(app).get(`/cars/${carMock._id}`).send();
   
       expect(chaiHttpResponse).to.have.status(200);
-    });
-  
-    it('should return a car when the id is valid', async () => {
-      const chaiHttpResponse = await chai.request(app).get(`/cars/${carMock._id}`).send(carBodyMock);
-  
       expect(chaiHttpResponse.body).to.be.deep.eq(carMock);
     });
-
-    it('should return status 400 when the id is invalid', async () => {
+    
+    it('should return status 400 and an invalid id error message when the id is invalid', async () => {
       const chaiHttpResponse = await chai.request(app).get(`/cars/123456`).send(invalidCarBodyMock);
-  
+
       expect(chaiHttpResponse).to.have.status(400);
+      expect(chaiHttpResponse.body.error).to.be.eq('Id must have 24 hexadecimal characters');
+    });
+
+    it('should return status 500 and an "Internal Server Error" message', async () => {
+      const chaiHttpResponse = await chai.request(app).get(`/cars/${carMock._id}`).send(carBodyMock);
+  
+      expect(chaiHttpResponse).to.have.status(500);
+      expect(chaiHttpResponse.body.error).to.be.eq('Internal Server Error');
     });
   });
 
   describe('update', () => {
-    before(() => Sinon.stub(carModel.model, 'findByIdAndUpdate').resolves({ ...carMock, doorsQty: 4 } as any));
+    before(() => Sinon.stub(carModel.model, 'findByIdAndUpdate')
+    .onFirstCall().resolves({ ...carMock, doorsQty: 4 } as any)
+    .onSecondCall().throws()
+    .onThirdCall().throws());
   
     after(()=> Sinon.restore());
   
-    it('should return status 200 when the id and data are valid', async () => {
+    it('should return status 200 and an updated car when the id and data are valid', async () => {
       const chaiHttpResponse = await chai.request(app).put(`/cars/${carMock._id}`).send({ doorsQty: 4 });
 
       expect(chaiHttpResponse).to.have.status(200);
-    });
-  
-    it('should return an updated car created when the id data are valid', async () => {
-      const chaiHttpResponse = await chai.request(app).put(`/cars/${carMock._id}`).send({ doorsQty: 4 });
-  
       expect(chaiHttpResponse.body).to.be.deep.eq({ ...carMock, doorsQty: 4 });
     });
-
-    it('should return status 400 when the id is invalid', async () => {
-      const chaiHttpResponse = await chai.request(app).put('/cars/123456').send({ doorsQty: 4 });
   
+    
+    it('should return status 400 and an invalid id error message when the id is invalid', async () => {
+      const chaiHttpResponse = await chai.request(app).put('/cars/123456').send({ doorsQty: 4 });
+
       expect(chaiHttpResponse).to.have.status(400);
+      expect(chaiHttpResponse.body.error).to.be.eq('Id must have 24 hexadecimal characters');
+    });
+
+    it('should return status 500 and an "Internal Server Error" message', async () => {
+      const chaiHttpResponse = await chai.request(app).put(`/cars/${carMock._id}`).send({ doorsQty: 4 });
+  
+      expect(chaiHttpResponse).to.have.status(500);
+      expect(chaiHttpResponse.body.error).to.be.eq('Internal Server Error');
     });
   });
 
   describe('delete', () => {
-    before(() => Sinon.stub(carModel.model, 'findByIdAndDelete').resolves({ ...carMock, doorsQty: 4 } as any));
+    before(() => Sinon.stub(carModel.model, 'findByIdAndDelete')
+      .onFirstCall().resolves({ ...carMock, doorsQty: 4 } as any)
+      .onSecondCall().throws()
+      .onThirdCall().throws());
   
     after(()=> Sinon.restore());
   
@@ -123,18 +149,21 @@ describe('CarController', () => {
       const chaiHttpResponse = await chai.request(app).delete(`/cars/${carMock._id}`).send();
 
       expect(chaiHttpResponse).to.have.status(204);
-    });
-  
-    it('should return the car deleted when the id is valid', async () => {
-      const chaiHttpResponse = await chai.request(app).delete(`/cars/${carMock._id}`).send();
-  
       expect(chaiHttpResponse.body).to.be.empty;
     });
-
-    it('should return status 400 when the id is invalid', async () => {
+    
+    it('should return status 400 and an invalid id error message when the id is invalid', async () => {
       const chaiHttpResponse = await chai.request(app).delete('/cars/123456').send();
-  
+      
       expect(chaiHttpResponse).to.have.status(400);
+      expect(chaiHttpResponse.body.error).to.be.eq('Id must have 24 hexadecimal characters');
+    });
+
+    it('should return status 500 and an "Internal Server Error" message', async () => {
+      const chaiHttpResponse = await chai.request(app).delete(`/cars/${carMock._id}`).send();
+  
+      expect(chaiHttpResponse).to.have.status(500);
+      expect(chaiHttpResponse.body.error).to.be.eq('Internal Server Error');
     });
   });
 });
@@ -143,97 +172,122 @@ describe('MotorcycleController', () => {
   const motorcycleModel = new MotorcycleModel();
 
   describe('create', () => {
-    before(() => Sinon.stub(motorcycleModel.model, 'create').resolves(motoMock));
+    before(() => Sinon.stub(motorcycleModel.model, 'create')
+      .onFirstCall().resolves(motoMock)
+      .onSecondCall().throws()
+      .onThirdCall().throws());
   
     after(()=> Sinon.restore());
   
-    it('should return status 201 when data is valid', async () => {
+    it('should return status 201 and a new motorcycle created when data is valid', async () => {
       const chaiHttpResponse = await chai.request(app).post('/motorcycles').send(motoBodyMock);
   
       expect(chaiHttpResponse).to.have.status(201);
-    });
-  
-    it('should return a new car created when data is valid', async () => {
-      const chaiHttpResponse = await chai.request(app).post('/motorcycles').send(motoBodyMock);
-  
       expect(chaiHttpResponse.body).to.be.deep.eq(motoMock);
     });
-
-    it('should return status 400 when data is invalid', async () => {
+    
+    it('should return status 400 and a ZodError message when data is invalid', async () => {
       const chaiHttpResponse = await chai.request(app).post('/motorcycles').send(invalidMotoBodyMock);
-  
+      
       expect(chaiHttpResponse).to.have.status(400);
+      expect(chaiHttpResponse.body.error.name).to.be.eq('ZodError');
     });
+    
+      it('should return status 500 and an "Internal Server Error" message', async () => {
+        const chaiHttpResponse = await chai.request(app).post('/motorcycles').send(motoBodyMock);
+    
+        expect(chaiHttpResponse).to.have.status(500);
+        expect(chaiHttpResponse.body.error).to.be.eq('Internal Server Error');
+      });
   });
 
   describe('read', () => {
-    before(() => Sinon.stub(motorcycleModel.model, 'find').resolves(motoListMock as any[]));
+    before(() => Sinon.stub(motorcycleModel.model, 'find')
+      .onFirstCall().resolves(motoListMock as any[])
+      .onSecondCall().throws());
   
     after(()=> Sinon.restore());
   
-    it('should return status 200', async () => {
+    it('should return status 200 and a list of all motorcycles', async () => {
       const chaiHttpResponse = await chai.request(app).get('/motorcycles').send();
   
       expect(chaiHttpResponse).to.have.status(200);
+      expect(chaiHttpResponse.body).to.be.deep.eq(motoListMock);
     });
   
-    it('should return a list of all motorcycles', async () => {
+    it('should return status 500 and an "Internal Server Error" message', async () => {
       const chaiHttpResponse = await chai.request(app).get('/motorcycles').send();
-  
-      expect(chaiHttpResponse.body).to.be.deep.eq(motoListMock);
+
+      expect(chaiHttpResponse).to.have.status(500);
+      expect(chaiHttpResponse.body.error).to.be.eq('Internal Server Error');
     });
   });
 
   describe('readOne', () => {
-    before(() => Sinon.stub(motorcycleModel.model, 'findById').resolves(motoMock as any));
+    before(() => Sinon.stub(motorcycleModel.model, 'findById')
+      .onFirstCall().resolves(motoMock as any)
+      .onSecondCall().throws()
+      .onThirdCall().throws());
   
     after(()=> Sinon.restore());
   
-    it('should return status 200 when the id is valid', async () => {
+    it('should return status 200 and a motorcycle when the id is valid', async () => {
       const chaiHttpResponse = await chai.request(app).get(`/motorcycles/${motoMock._id}`).send();
   
       expect(chaiHttpResponse).to.have.status(200);
-    });
-  
-    it('should return a motorcycle when the id is valid', async () => {
-      const chaiHttpResponse = await chai.request(app).get(`/motorcycles/${motoMock._id}`).send(motoBodyMock);
-  
       expect(chaiHttpResponse.body).to.be.deep.eq(motoMock);
     });
-
-    it('should return status 400 when the id is invalid', async () => {
+    
+    it('should return status 400 and an invalid id error message when the id is invalid', async () => {
       const chaiHttpResponse = await chai.request(app).get(`/motorcycles/123456`).send(invalidMotoBodyMock);
-  
+      
       expect(chaiHttpResponse).to.have.status(400);
+      expect(chaiHttpResponse.body.error).to.be.eq('Id must have 24 hexadecimal characters');
     });
+    
+      it('should return status 500 and an "Internal Server Error" message', async () => {
+        const chaiHttpResponse = await chai.request(app).get(`/motorcycles/${motoMock._id}`).send(motoBodyMock);
+    
+        expect(chaiHttpResponse).to.have.status(500);
+        expect(chaiHttpResponse.body.error).to.be.eq('Internal Server Error');
+      });
   });
 
   describe('update', () => {
-    before(() => Sinon.stub(motorcycleModel.model, 'findByIdAndUpdate').resolves({ ...motoMock, doorsQty: 4 } as any));
+    before(() => Sinon.stub(motorcycleModel.model, 'findByIdAndUpdate')
+      .onFirstCall().resolves({ ...motoMock, doorsQty: 4 } as any)
+      .onSecondCall().throws()
+      .onThirdCall().throws());
   
     after(()=> Sinon.restore());
   
-    it('should return status 200 when the id and data are valid', async () => {
+    it('should return status 200 and an updated motorcycle when the id and data are valid', async () => {
       const chaiHttpResponse = await chai.request(app).put(`/motorcycles/${motoMock._id}`).send({ doorsQty: 4 });
 
       expect(chaiHttpResponse).to.have.status(200);
-    });
-  
-    it('should return an updated motorcycle created when the id data are valid', async () => {
-      const chaiHttpResponse = await chai.request(app).put(`/motorcycles/${motoMock._id}`).send({ doorsQty: 4 });
-  
       expect(chaiHttpResponse.body).to.be.deep.eq({ ...motoMock, doorsQty: 4 });
     });
-
-    it('should return status 400 when the id is invalid', async () => {
+    
+    it('should return status 400 and an invalid id error message when the id is invalid', async () => {
       const chaiHttpResponse = await chai.request(app).put('/motorcycles/123456').send({ doorsQty: 4 });
-  
+      
       expect(chaiHttpResponse).to.have.status(400);
+      expect(chaiHttpResponse.body.error).to.be.eq('Id must have 24 hexadecimal characters');
     });
+    
+      it('should return status 500 and an "Internal Server Error" message', async () => {
+        const chaiHttpResponse = await chai.request(app).put(`/motorcycles/${motoMock._id}`).send({ doorsQty: 4 });
+    
+        expect(chaiHttpResponse).to.have.status(500);
+        expect(chaiHttpResponse.body.error).to.be.eq('Internal Server Error');
+      });
   });
 
   describe('delete', () => {
-    before(() => Sinon.stub(motorcycleModel.model, 'findByIdAndDelete').resolves({ ...motoMock, doorsQty: 4 } as any));
+    before(() => Sinon.stub(motorcycleModel.model, 'findByIdAndDelete')
+      .onFirstCall().resolves({ ...motoMock, doorsQty: 4 } as any)
+      .onSecondCall().throws()
+      .onThirdCall().throws());
   
     after(()=> Sinon.restore());
   
@@ -241,44 +295,21 @@ describe('MotorcycleController', () => {
       const chaiHttpResponse = await chai.request(app).delete(`/motorcycles/${motoMock._id}`).send();
 
       expect(chaiHttpResponse).to.have.status(204);
-    });
-  
-    it('should return the motorcycle deleted when the id is valid', async () => {
-      const chaiHttpResponse = await chai.request(app).delete(`/motorcycles/${motoMock._id}`).send();
-  
       expect(chaiHttpResponse.body).to.be.empty;
     });
-
-    it('should return status 400 when the id is invalid', async () => {
+    
+    it('should return status 400 and an invalid id error message when the id is invalid', async () => {
       const chaiHttpResponse = await chai.request(app).delete('/motorcycles/123456').send();
-  
+
       expect(chaiHttpResponse).to.have.status(400);
+      expect(chaiHttpResponse.body.error).to.be.eq('Id must have 24 hexadecimal characters');
     });
+    
+      it('should return status 500 and an "Internal Server Error" message', async () => {
+        const chaiHttpResponse = await chai.request(app).delete(`/motorcycles/${motoMock._id}`).send();
+    
+        expect(chaiHttpResponse).to.have.status(500);
+        expect(chaiHttpResponse.body.error).to.be.eq('Internal Server Error');
+      });
   });
 });
-
-// describe('CarController', () => {
-//   let carController = new CarController();
-
-//   describe('create', () => {
-//     const req = { } as IRequestWithBody<Car>;
-//     const res = { } as Response;
-  
-//     before(() => {
-//       req.body = carBodyMock;
-//       res.status = Sinon.stub().returns(res);
-//       res.json = Sinon.stub().returns(null);
-
-//       Sinon.stub(carController, 'create').resolves(carMock as any);
-//     });
-  
-//     after(()=> Sinon.restore());
-  
-//     it('should return status 201', async () => {
-//       // const chaiHttpResponse = await chai.request(server).post('/cars').send(carBodyMock);
-//       await carController.create(req, res);
-
-//       expect((res.status as Sinon.SinonStub).calledWith(201)).to.be.eq(true);
-//     });
-//   });
-// });
